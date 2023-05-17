fcbecf8712cab19d3bd684adb7da8ddcb3a3912e
Aaditya785 <aadityasandil5@gmail.com>
2023-05-17 17:11:16 +0530

    Completed the Auth Component

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,4 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import "../assets/Cart.css"
 
 
 export const Checkout = () => {
@@ -7,7 +8,8 @@ export const Checkout = () => {
   
   return (
     <> 
-    {isAuthenticated ? (<h1 style={{fontSize:"50px"}}>Hii!,<br/>{user.name}<br/>Your Item Delivered Sortly</h1>):(<div><h1>You Have To Login/signup First</h1><br/><button onClick={()=> loginWithRedirect()}>Login/Signup</button></div>)}
+    {isAuthenticated ? (<h1 style={{fontSize:"50px"}}>Hii!,<br/>{user.name}<br/>Your Item Delivered Sortly</h1>)
+    :(<div classname="checkLogin" ><h1>You Have To Login/signup First</h1><br/><button onClick={()=> loginWithRedirect()}>Login/Signup</button></div>)}
     </>
   );
 };
